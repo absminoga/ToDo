dc511f2a57aad25b94be2c8a1b3dc04a2bcc70ee
Сергій Зубрін <absminoga@gmail.com>
2020-03-15 22:22:54 +0200

    ADD task js

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,7 +8,6 @@ jsTriggers.forEach(function(trigger) {
            content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
            activeTrigger = document.querySelector('.js-tab-trigger.active'),
            activeContent = document.querySelector('.js-tab-content.active');
-       console.log(this);
        
        activeTrigger.classList.remove('active');
        trigger.classList.add('active');
@@ -21,7 +20,9 @@ jsTriggers.forEach(function(trigger) {
 class ToDoList{
     constructor (){
         this.addButton = document.querySelector('.add_btn');
+        this.inputTitle = document.getElementById('new_title');
         this.inputTask = document.getElementById('new_task');
+        this.inputDate = document.querySelector('.date_field');
         this.incompletedTask = document.querySelector('.incompleted_tasks');
         this.textArea = document.getElementById('new_title');
         this.addButton.onclick = () => this.addTask();
@@ -30,27 +31,63 @@ class ToDoList{
         this.removedTask = null;
         
     }
-    creatureNewItem(task){
-        let listItem = document.createElement('li');
+    creatureNewItem(title, task, date){
+        let listItem = document.createElement('div');
+        listItem.classList.add('task_card');
+
         let checkbox = document.createElement('input');
         checkbox.setAttribute('type', 'checkbox');
-        let label = document.createElement('label');
-        label.innerText = task;
-        let input = document.createElement('input');
-        input.setAttribute('type', 'text'); 
-        input.setAttribute('disabled', ''); 
+        
+        let textDescription = document.createElement('div');
+        textDescription.classList.add('block_text_description');
+        
+        let inputTitle = document.createElement('textarea');
+        inputTitle.setAttribute('type', 'text'); 
+        inputTitle.classList.add('input_title');
+        inputTitle.setAttribute('disabled', ''); 
+        inputTitle.innerText = title;
+        
+        let inputDescription = document.createElement('textarea');
+        inputDescription.setAttribute('type', 'text'); 
+        inputDescription.classList.add('input_description');
+        inputDescription.setAttribute('disabled', ''); 
+        inputDescription.innerText = task;
+
+        let dateCompletion = document.createElement('div');
+        dateCompletion.classList.add('date_completion');
+
+        let dateField = document.createElement('textarea');
+        dateField.setAttribute('type', 'number'); 
+        dateField.classList.add('date_field');
+        dateField.setAttribute('disabled', '');
+        dateField.innerText = date;
+
+        let calendar = document.createElement('div');
+        calendar.classList.add('calendar');
+
+        let calendarBtn = document.createElement('button');
+        calendarBtn.classList.add('calendar_btn');
+        calendarBtn.innerText = 'change date';
+
         let buttonItem = document.createElement('div');
         buttonItem.classList.add('button_item');
+
         let buttonEdit = document.createElement('button');
         buttonEdit.classList.add('edit');
         buttonEdit.innerText = 'edit';
+
         let buttonDelete = document.createElement('button');
         buttonDelete.classList.add('delete');
         buttonDelete.innerText = 'delete';
         
         listItem.appendChild(checkbox);
-        listItem.appendChild(label);
-        listItem.appendChild(input);
+        listItem.appendChild(textDescription);
+        textDescription.appendChild(inputTitle);
+        textDescription.appendChild(inputDescription);
+        textDescription.appendChild(dateCompletion);
+        dateCompletion.appendChild(dateField);
+        dateCompletion.appendChild(calendar);
+        dateCompletion.appendChild(calendarBtn);
         listItem.appendChild(buttonItem)
         buttonItem.appendChild(buttonEdit);
         buttonItem.appendChild(buttonDelete);
@@ -65,11 +102,13 @@ class ToDoList{
         if (this.textArea.value.length > 30) {
             this.textArea.setAttribute("id", "warning");
             this.addButton.setAttribute('disabled', '');
-            textCounter.classList.add('warning')
+            textCounter.classList.add('warning');
+            this.addButton.classList.add('btn_warning');
           } else{   
             this.textArea.setAttribute("id", "new_title");
             this.addButton.removeAttribute('disabled', '');
             textCounter.classList.remove('warning');
+            this.addButton.classList.remove('btn_warning');
           }
     }
 
@@ -85,11 +124,16 @@ class ToDoList{
     }
     
     addTask(listItem){
-        if(this.inputTask.value){
-            let listItem = this.creatureNewItem(this.inputTask.value);
+        console.log(this.inputDate.value);
+        
+        if(this.inputTask.value && this.inputTitle.value && this.inputDate.value){
+            console.log("Add");
+            let listItem = this.creatureNewItem(this.inputTitle.value, this.inputTask.value, this.inputDate.value);
             this.incompletedTask.appendChild(listItem);
             this.buttonTaskEvents (listItem); 
+            this.inputTitle.value = ''; 
             this.inputTask.value = ''; 
+            this.inputDate.value = '';
         } 
     }
      
