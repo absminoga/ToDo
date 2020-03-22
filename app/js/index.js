5d4ccc76f238ed77a1ead19dcfcdfb57ee2eaf68
Сергій Зубрін <absminoga@gmail.com>
2020-03-22 19:59:33 +0200

    delete all  calendar

--- app/js/index.js
+++ app/js/index.js
@@ -16,65 +16,9 @@ jsTriggers.forEach(function(trigger) {
        content.classList.add('active');
     });
  });
-<<<<<<< HEAD
 //  Calendar
-function Calendar3(id, year, month) {
-    var Dlast = new Date(year,month+1,0).getDate(),
-        D = new Date(year,month,Dlast),
-        DNlast = D.getDay(),
-        DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
-        calendar = '<tr>',
-        m = document.querySelector('#'+id+' option[value="' + D.getMonth() + '"]'),
-        g = document.querySelector('#'+id+' input');
-    if (DNfirst != 0) {
-      for(var  i = 1; i < DNfirst; i++) calendar += '<tdclass="date_fields">';
-    }else{
-      for(var  i = 0; i < 6; i++) calendar += '<td>';
-    }
-    for(var  i = 1; i <= Dlast; i++) {
-      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-        calendar += '<td class="today date_field">' + i;
-      }else{
-        if (  // список официальных праздников
-            (i == 1 && D.getMonth() == 0 && ((D.getFullYear() > 1897 && D.getFullYear() < 1930) || D.getFullYear() > 1947)) || // Новый год
-            (i == 2 && D.getMonth() == 0 && D.getFullYear() > 1992) || // Новий Рік
-            ((i == 3 || i == 4 || i == 5 || i == 6 || i == 8) && D.getMonth() == 0 && D.getFullYear() > 2004) || // Новий Рік
-            (i == 7 && D.getMonth() == 0 && D.getFullYear() > 1990) || // Різдво
-            (i == 8 && D.getMonth() == 2 && D.getFullYear() > 1965) || // 8 Березня
-            (i == 1 && D.getMonth() == 4 && D.getFullYear() > 1917) || // 1 травня
-            (i == 9 && D.getMonth() == 4 && D.getFullYear() > 1964) // День Перемоги
-           ) {
-          calendar += '<td class="holiday date_fields">' + i;
-        }else{
-          calendar += '<td class="date_fields">' + i;
-        }
-      }
-      if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
-        calendar += '<tr>';
-      }
-    }
-    for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-    document.querySelector('#'+id+' tbody').innerHTML = calendar;
-    g.value = D.getFullYear();
-    m.selected = true;
-    if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {
-        document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
-    }
-    document.querySelector('#'+id+' option[value="' + new Date().getMonth() + '"]').style.color = 'rgb(220, 0, 0)'; // в выпадающем списке выделен текущий месяц
-    }
-    Calendar3("calendar3",new Date().getFullYear(),new Date().getMonth());
-    document.querySelector('#calendar3').onchange = function Kalendar3() {
-      Calendar3("calendar3",document.querySelector('#calendar3 input').value,parseFloat(document.querySelector('#calendar3 select').options[document.querySelector('#calendar3 select').selectedIndex].value));
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
->>>>>>> parent of 7ca1e8d3... add calendar
+
 // Add task
 class ToDoList{
     constructor (){
@@ -87,57 +31,23 @@ class ToDoList{
         this.textCounter = document.querySelector('.text-counter');
         this.textArea = document.getElementById('new_title');
         this.taskCounter = document.querySelectorAll('.task_counter');
-<<<<<<< HEAD
         this.calendarBtn = document.querySelector('.calendar_btn');
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // this.taskDate = document.querySelectorAll('.date_fields');
-=======
-        this.taskDate = document.querySelectorAll('.date_fields')
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
-        this.taskDate = document.querySelectorAll('.date_fields')
->>>>>>> parent of c09fb1ef... Add new calendar
+        this.taskDate = document.querySelectorAll('.date_fields');
 
     
         this.addButton.onclick = () =>  this.addTask();
         this.calendarBtn.onclick = () =>  this.addCalendar();
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // this.taskDate.onclick = () =>  this.inputDate();
-=======
         this.taskDate.onclick = () =>  this.inputDate();
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
-        this.taskDate.onclick = () =>  this.inputDate();
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
-    
-        this.addButton.onclick = () =>  this.addTask();
->>>>>>> parent of 7ca1e8d3... add calendar
         this.textArea.oninput = () => this.changeTitle();
+        
         setInterval(this.getDate, 0);
         this.removedTask = null;    
         
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         // this.taskDate.forEach(e => {
         //     e.addEventListener('click', date => {
                 
         //     });
         // });     
-=======
-        console.log(this.taskDate);
-        
->>>>>>> parent of c09fb1ef... Add new calendar
-=======
-        console.log(this.taskDate);
-=======
-        console.log(this.taskCounter);
->>>>>>> parent of 7ca1e8d3... add calendar
-        
->>>>>>> parent of c09fb1ef... Add new calendar
     }
     creatureNewItem(title, task, date){
         let listItem = document.createElement('div');
@@ -223,6 +133,15 @@ class ToDoList{
        console.log('changeСounterTask');
        
     }
+    addCalendar(){
+        let calendar = document.getElementById('calendar3');
+       calendar.classList.toggle('calendar_card'); 
+    }
+
+    inputDate(){
+        console.log("Input date");
+        
+    }
 
     buttonTaskEvents(listItem){ 
         let editButton = listItem.querySelector('button.edit');
@@ -264,8 +183,7 @@ class ToDoList{
              editButton.innerText = "edit";
              changeTitle.setAttribute('disabled', '');
              changeDescription.setAttribute('disabled', '');
-             changeDate.setAttribute('disabled', '');
-                
+             changeDate.setAttribute('disabled', '');      
          } else{
             editButton.innerText = "save";
             changeTitle.removeAttribute('disabled', '');
